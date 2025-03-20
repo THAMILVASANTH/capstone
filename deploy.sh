4de4dac3e856eeaa55cbfb2e9f535524709672ca
THAMIL VASANTH G K <126443048+THAMILVASANTH@users.noreply.github.com>
2025-03-20 15:28:30 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u thamilvasanth -p dckr_pat_4VneGvGgUIQa_WA_SmRXNKwB7bw
-    docker tag test thamilvasanth/devops
+    docker tag test1 thamilvasanth/devops
     docker push thamilvasanth/devops
     docker-compose up -d
     
